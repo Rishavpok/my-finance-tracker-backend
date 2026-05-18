4133ac0f7d765a32aa6dc55c5c6c17097a561179
rishav <rishavpokharel98@gmail.com>
2026-05-18 09:27:59 +0545

    main file updated

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
- app.enableCors({
-    origin: 'http://localhost:3001', // your Next.js frontend
-    credentials: true,
-  });
+    const port = process.env.PORT || 4000;
+ app.enableCors();
 
-  await app.listen(4000);
+  await app.listen(port);
 }
 bootstrap();
